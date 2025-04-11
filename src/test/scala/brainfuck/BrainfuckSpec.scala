1b24f5ef89328d32ec4511ace60cbf63d077f626
YONEZAWA Izumi <izumi@nurs.or.jp>
2025-04-11 22:06:18 +0900

    update: update successful cases and add failure cases

--- src/test/scala/brainfuck/BrainfuckSpec.scala
+++ src/test/scala/brainfuck/BrainfuckSpec.scala
@@ -3,14 +3,14 @@ class BrainfuckSpec extends munit.FunSuite {
   test("ハロワ") {
     val source = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
     val result = (new Brainfuck).run(source, null)
-    assert(result.isRight)
+    assert(result.isSuccess)
     assertEquals(new String(result.toOption.get.toArray), "Hello World!\n")
   }
 
   test("全バイトダンプ") {
     val source = ".+[.+]"
     val result = (new Brainfuck).run(source, null)
-    assert(result.isRight)
+    assert(result.isSuccess)
     assertEquals(result.toOption.get.map(_.toInt), (Range(0, 128) ++ Range(-128, 0)).toList)
   }
 
@@ -18,8 +18,22 @@ class BrainfuckSpec extends munit.FunSuite {
     val source = ",[.,]"
     val input = Range(33, 36).map(_.toByte)
     val result = (new Brainfuck).run(source, input)
-    assert(result.isRight)
+    assert(result.isSuccess)
     assertEquals(result.toOption.get, input)
+  }
+
+  test("excessive open") {
+    val source = "++["
+    val result = (new Brainfuck).run(source, null)
+    assert(result.isFailure)
+    assertEquals(result.toEither.swap.toOption.get.getMessage, "loop overflow")
+  }
+
+  test("excessive close") {
+    val source = "++][]"
+    val result = (new Brainfuck).run(source, null)
+    assert(result.isFailure)
+    assertEquals(result.toEither.swap.toOption.get.getMessage, "loop underflow")
   }
 
 }
